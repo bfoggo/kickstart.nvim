9b20f41d13804a56d0a1397963a2b9529b7475a8
bfoggo <brandon.foggo@habitat.energy>
2023-06-17 18:17:05 -0500

    gave remaps for go back and go forward since the defaults interferred with zellij

--- init.lua
+++ init.lua
@@ -413,8 +413,9 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gb', '<C-o>', '[G]o [B]ack')
+  nmap('gf', '<C-i>', '[G]o [F]orward')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
